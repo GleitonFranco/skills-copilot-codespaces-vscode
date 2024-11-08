1cb71cc060e10dc61ac420c965816c757747f51b
GleitonFranco <gleitonfranco@gmail.com>
2024-11-08 01:10:32 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,18 +2,3 @@
 
 const http = require('http');
 
-http.createServer((req, res) => {
-  if (req.url === '/comments') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    const comments = [
-      { username: 'Bob', comment: 'I love your blog!' },
-      { username: 'Dave', comment: 'Good job' },
-    ];
-    res.end(JSON.stringify(comments));
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
-}).listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
