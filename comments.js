39c8b4cffe918e34df88b5f65832b27c211fa333
GleitonFranco <gleitonfranco@gmail.com>
2024-11-08 01:15:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,18 +2,4 @@
 // Create a route for POST /comments
 // Add a comment to the comments array
 // Respond with a status code of 201
-
-// Create a new route for POST /comments
-app.post('/comments', (req, res) => {
-  // Get the comment from the request body
-  const comment = req.body.comment;
-
-  // Add the comment to the comments array
-  comments.push(comment);
-
-  // Respond with a status code of 201
-  res.status(201).send();
-});
-
-// Export the comments array
-module.exports = comments;
+console.log('comments.js');
